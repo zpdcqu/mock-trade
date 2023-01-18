9334a5f558a24f86833450d2b141c1a3fc1adc73
zpdcqu@163.com <Wszpd602>
2023-01-18 15:14:24 +0800

    初始化

---
+++ src/main/java/com/pdongzheng/mocktrade/MocktradeApplication.java
@@ -0,0 +1,13 @@
+package com.pdongzheng.mocktrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocktradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocktradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdongzheng/mocktrade/MocktradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdongzheng.mocktrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocktradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
