d2722392d5149488b745ef8b69744000ab808f89
zpdcqu <zpdcqu@163.com>
2023-01-21 23:21:07 +0800

    自动创建表

--- src/main/java/com/pdongzheng/mocktrade/MocktradeApplication.java
+++ src/main/java/com/pdongzheng/mocktrade/MocktradeApplication.java
@@ -1,9 +1,13 @@
 package com.pdongzheng.mocktrade;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan({"com.gitee.sunchenbin.mybatis.actable.dao.*"} )
+@ComponentScan("com.gitee.sunchenbin.mybatis.actable.manager.*")
 public class MocktradeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pdongzheng/mocktrade/po/AccountPO.java
+++ src/main/java/com/pdongzheng/mocktrade/po/AccountPO.java
@@ -2,24 +2,30 @@ package com.pdongzheng.mocktrade.po;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.*;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlCharsetConstant;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlEngineConstant;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@TableName("account")
+@Table(value = "account", comment = "账户",isSimple = true)
+@TableCharset(MySqlCharsetConstant.UTF8)
+@TableEngine(MySqlEngineConstant.InnoDB)
 public class AccountPO {
 
-    @TableId
+    @IsKey
+    @IsAutoIncrement
     private Long id;
-
+    @Column
     private String name;
-
+    @Column
     private Long balance;
-
+    @Column
     private String desc;
-
+    @Column
     private LocalDateTime createTime;
-
+    @Column
     private LocalDateTime updateTime;
 }
